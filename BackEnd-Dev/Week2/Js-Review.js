f921de3c837a3dae9e52b3137571c5676137fa12
Karam Erenat <62215059+KaramErenat@users.noreply.github.com>
2024-12-17 19:25:13 +0300

    DevTrack: Files: Js-Review.js, test.html, tempCodeRunnerFile.js.  | Changes in: Js-Review.js; test.html (modified console; added sum, return, x, y, z, for; removed http, hostname, port, server, res); tempCodeRunnerFile.js (modified console; added sum, return, x, y, z, for; removed http, hostname, port, server, res)

--- BackEnd-Dev/Week2/Js-Review.js
+++ BackEnd-Dev/Week2/Js-Review.js
@@ -1,14 +1,37 @@
-const http = require('http');
+// 1. Write a JavaScript function to calculate the sum of two numbers. 
+function sum(num1,num2){
+    return num1+num2;
+}
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//2. Write a JavaScript function to check if a given string is a palindrome.
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+let x = "Karam"
+let y = x.split("")
+console.log(y.length);
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+let z =[]
+for (let i = y.length-1; i >=0; i--) {
+    console.log(y[i]);
+    z.push
+    
+    
+}
+
+
+
+// function palindrome (userString){
+
+//     let splitedString = userString.split("")
+//     let reverseString = [];
+
+//     for(i = splitedString.length; i>=0;i--){
+//         reverseString.push
+//     }
+
+//     let finalString = reverseString.join("")
+//     return finalString;
+// }
+
+
+// console.log(palindrome("karam"));
+
---
+++ BackEnd-Dev/Week2/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+function palindrome (userString){
+
+//     let splitedString = userString.split("")
+//     let reverseString = [];
+
+//     for(i = splitedString.length; i>=0;i--){
+//         reverseString.push
+//     }
+
+//     let finalString = reverseString.join("")
+//     return finalString;
+// }
+
+
+// console.log(palindrome("karam"));
