29ae604ae73e1e2d4abea25f4dfe5986bea9705a
Karam Erenat <62215059+KaramErenat@users.noreply.github.com>
2024-12-22 21:41:49 +0300

    DevTrack: Files: First-http-server.js.  | Changes in: First-http-server.js

--- BackEnd-Dev/Week2/First-http-server.js
+++ BackEnd-Dev/Week2/First-http-server.js
@@ -1,3 +1,27 @@
-console.log("hello World");
-console.log("hello world 2");
-console.log("hello world 3");
+const http = require('http')
+
+
+// Array
+const users = [
+    {name: "Ahmed", age: 25},
+    {name: "Ali", age: 30},
+    {name: "Omar", age: 35},
+    {name: "Sara", age: 20},
+    {name: "Sama", age: 22}
+]
+
+
+
+http.createServer(
+    (req,res)=>{
+        if(req.url == '/' && req.method == 'GET'){
+            res.write(JSON.stringify(users))
+            res.end()
+            
+        }else{
+            console.log("error 404");
+            
+        }
+    }
+).listen(3000, ()=>console.log("Server is working"));
+
